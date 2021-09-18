074ac713f8ddc08088d7c29f8177c04c9de72177
Ruben Cano Fransua <headmaster21@gmail.com>
2021-09-18 00:23:19 -0400

    Add project files.

---
+++ MyIVProject/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIVProject
+{
+    public static class Helpers
+    {
+        
+        public static List<int> Pares( this List<int> l)
+        {
+            Predicate<int> EsPar = x => x % 2 == 0;
+            return l.FindAll(EsPar);
+        }
+
+        public static List<int> Impares(this List<int> l)
+        {
+            Predicate<int> EsImpar = x => x % 2 != 0;
+            return l.FindAll(EsImpar);
+
+
+        }
+    }
+}
---
+++ MyIVProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyIVProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Lista = new List<int>() { 1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            Console.WriteLine("Imprimiendo Valores Pares ");
+            Console.WriteLine("=========================");
+
+            foreach (int i in Lista.Pares())
+            {
+                Console.WriteLine("Para : "+ i.ToString());
+            }
+
+
+            Console.WriteLine("Imprimiendo Valores Imares ");
+            Console.WriteLine("=========================");
+
+            foreach (int i in Lista.Impares())
+            {
+                Console.WriteLine("Para : " + i.ToString());
+            }
+
+        }
+    }
+}
